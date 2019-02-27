878df27ef67bf821b828a718bae571e1c376943e
Graham West <gtw2i@mtmail.mtsu.edu>
2019-02-27 14:49:15 -0600

    searchTree prototype weirdness

--- code/kdTree/serial/searchTree.cpp
+++ code/kdTree/serial/searchTree.cpp
@@ -1,59 +1,13 @@
 //
-// importFiles
-//
-
-
-#undef _DEBUG_
-
-
-//
-// STL includes
-//
-
-#include <vector>
-
-//
-// Standard includes
-//
-
-#include <string>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-
-
-//
 // Local includes
 //
-//
 
-#include "definitions.h"
-// #include "Data.h"
+#include "Tree.h"
 
-//
-// Function: searchTree
-//
 
-// THIS FILE JUST CONTAINS COMMENTS FOR NOW
-void searchTree( double *found, Tree tree, double *c0, double rad ) {
-	// rad is the circle radius
-	// c0 is the center of the circle
+int Tree::SearchTree(double x, double y, double z, double radius)
+{
 	
-	// get get range
-	
-	// check for intersection
-	
-	// if intersecting
-	 	
-		// if n > 1
-			// searchTree( foundL, tree.L, c0, rad )
-			// searchTree( foundR, tree.R, c0, rad )
-			// found = concat( foundL, foundR )
-		// else
-			// found = tree.data
-		// end
-		
-	// end
-	
+
+	return 0;
 }
-
