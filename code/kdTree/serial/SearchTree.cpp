e09fa7a2cc252af2d807296ae82f1ea3ca356735
JJ Lay <JJ.Lay@tmcat.com>
2019-02-27 13:33:02 -0600

    Rough prototype

---
+++ code/kdTree/serial/SearchTree.cpp
@@ -0,0 +1,13 @@
+//
+// Local includes
+//
+
+#include "Tree.h"
+
+
+int Tree::SearchTree(double x, double y, double z, double radius)
+{
+	
+
+	return 0;
+}
--- code/kdTree/serial/main.cpp
+++ code/kdTree/serial/main.cpp
@@ -1,7 +1,30 @@
+//
+// Local includes
+//
 
+#include "Tree.h"
+#include "definitions.h"
+
+
+//
+// Standard includes
+//
+
+#include <string>
+
+using namespace std;
+
+
+//
+// Function:  main
+//
 
 int main(int argc, char *argv[]) {
 	
+	auto theTree = new Tree("datafile501.txt");
+
+	auto contained = theTree->SearchTree(0.0, 0.0, 0.0, 0.1);
 	
-	return 0;
+	return _OKAY_;
 }
+
