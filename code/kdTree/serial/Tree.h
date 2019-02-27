f5b6b93774f9e285faef6c3a061544c7f531c278
JJ Lay <JJ.Lay@tmcat.com>
2019-02-27 13:44:01 -0600

    Prototyped known functions

--- code/kdTree/serial/Tree.h
+++ code/kdTree/serial/Tree.h
@@ -11,13 +11,42 @@ using namespace std;
 
 class Tree {
 	private:
-		int dummy;
-	
-	
+		// Child nodes
+		Tree *left, *right;
+
+		// Parent node
+		Tree *parent;
+
+		double xMin, xMax, yMin, yMax, zMin, zMax;
+		double xCenter, yCenter, zCenter;
+		double radius;
+
+		int pointsContained;
+
+		int dimensionSorted;
+		double splitValue;
+
+		// For a single data point
+		int isDataPoint;
+		double x, y, z;
+		int index; // index from the row in the file
+
+		//
+		// Private methods that cannot be called outside of the class
+		//
+
+		double *SplitData(double *data, int numCols, int numRows);
+		void SortData(double *data, int numCols, int numRows);
+
+
 	public:
 		Tree(string filename);
 		Tree();
 		~Tree();
 		int SearchTree(double x, double y, double z, double radius);
+		double *GetRange();
+		double *GetCenter();
+		int GetCount();
+		int GetSortDimension();
 };
 
