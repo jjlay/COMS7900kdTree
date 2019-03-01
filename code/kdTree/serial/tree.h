c35d0c9647059d46983ebff3445caaf23a198dcc
JJ Lay <JJ.Lay@tmcat.com>
2019-03-01 11:29:15 -0600

    Created Makefile

---
+++ code/kdTree/serial/tree.h
@@ -0,0 +1,24 @@
+//
+// Struct
+//
+
+	// tree = struct( 'p',{}, 'l',{}, 'r',{}, 'i',{}, 'v',{}, 'x1',{}, 'x2',{}, 'y1',{}, 'y2',{}, 'n',{}, 'd', {}, 'c',{} );
+
+struct Tree {
+	Tree *p;  // Parent
+	Tree *l;  // Left child
+	Tree *r;  // Right child
+	int i;  // Sort index
+	
+	double x1; // Min x
+	double x2; // Max x
+	double y1;  // Min y
+	double y2;  // Max y
+	double z1;  // Min z
+	double z2;  // Max z
+	
+	double c[3]; // Center of this tree
+	double d[3];  // Data point
+	long unsigned int index;  // File row index
+}
+;
