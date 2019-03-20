d37d8bcaede2d9dd647b90d156885bb84890ae0a
JJ Lay <jj.lay@outlook.com>
2019-03-20 11:02:45 -0500

    Update importFiles.cpp

--- code/kdTree/parallelApproved/importFiles.cpp
+++ code/kdTree/parallelApproved/importFiles.cpp
@@ -14,6 +14,9 @@
 //
 
 #include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
 
 
 //
@@ -21,7 +24,9 @@
 //
 //
 
-#include "importData.h"
+#include "definitions.h"
+#include "Data.h"
+
 
 using namespace std;
 
@@ -32,18 +37,65 @@ using namespace std;
 //
 
 void importFiles(vector<string> files, int myRank,
-	double *myData, int *rows, int *cols, const int maxRowsPerFile,
-	const int arrayLimit) {
+	double *myData, int *rows, int *cols, int maxRows) {
 
+	*cols = _ROW_WIDTH_;
+
+	std::string homeDir = getenv("HOME");
+	std::string prefix = homeDir + "/localstorage/public/coms7900-data/datafile";
 	//const string prefix = "./data/datafile";
+	const string suffix = ".txt";
+	const double rowsPerFile = maxRows;
 	unsigned int arrayIndex = 0;
 	
 	// loop through files to read
 	for (auto f : files) {
-		importData(&myData[arrayIndex], f, cols, rows, 
-			maxRowsPerFile, arrayLimit);
-		arrayIndex = *rows * *cols;
-	}
-}
+		ifstream infile( f );
+		string line;
+		string token;
+		size_t pos = 0;
+		int lineCount = 0; //, tokenCount;
+		
+		
+		//
+		// Determine what the file number is
+		//
 
+		string extractedValue = f.substr(prefix.length(),
+			 f.length() - prefix.length() - suffix.length());
+		
+		double fileIndex = stod(extractedValue) - 1;
+		double totalLineCount = 1 + fileIndex * rowsPerFile;
+
+	
+		// loop through lines of file
+		while( getline(infile, line) and lineCount < maxRows ) {
+			
+			// add index
+			myData[arrayIndex * _ROW_WIDTH_ + _INDEX_] = totalLineCount;
+			
+			// add 1 double
+			token = line.substr(13, 21);
+			myData[arrayIndex * _ROW_WIDTH_ + _X_] = stod(token);
+
+			// add 2 double
+			token = line.substr(36, 21);
+			myData[arrayIndex * _ROW_WIDTH_ + _Y_] = stod(token);
+
+			token = line.substr(55, 21);
+			myData[arrayIndex * _ROW_WIDTH_ + _Z_] = stod(token);
+
+			lineCount++;
+			totalLineCount++;
+			arrayIndex++;
+		}
+
+
+	
+		// close the file
+		infile.close();
+	}
+
+	*rows = arrayIndex;
+}
 
