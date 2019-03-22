18171fe233fadcb53c83b0e936bbf45edd15494b
JJ Lay <JJ.Lay@tmcat.com>
2019-03-21 19:52:54 -0500

    debug 4

--- code/kdTree/parallelApproved/buildTree.cpp
+++ code/kdTree/parallelApproved/buildTree.cpp
@@ -32,10 +32,10 @@ void buildTree(double *data, int rows, int cols, Tree *tree, MPI_Comm comm, int 
 	int key = 50000 + (tree->depth) * 100;
 
 	if( q > 1 ) {
-		cout << key << " : " << __FUNCTION__ << " : Rank " << myRank << " parallel on communicator " << comm << endl;
+		cout << key << " : " << __FUNCTION__ << " : Depth " << tree->depth << " Rank " << myRank << " parallel on communicator " << comm << endl;
 		buildTree_parallel( data, rows, cols, tree, comm, myRank, q );
 	} else {
-		cout << key << " : " << __FUNCTION__ << " : Rank " << myRank << " serial " << " on communicataor " << comm << endl;
+		cout << key << " : " << __FUNCTION__ << " : Depth " << tree->depth << " Rank " << myRank << " serial " << " on communicataor " << comm << endl;
 		//buildTree_serial( data, rows, cols, tree);
 	}
 	
