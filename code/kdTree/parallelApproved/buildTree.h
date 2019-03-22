610959dc3774823b2f3f7b5e5222598ca647226e
JJ Lay <JJ.Lay@tmcat.com>
2019-03-21 20:21:47 -0500

    debug 11

--- code/kdTree/parallelApproved/buildTree.h
+++ code/kdTree/parallelApproved/buildTree.h
@@ -8,9 +8,18 @@
 
 
 //
+// Standard includes
+//
+
+#include <string>
+
+using namespace std;
+
+
+//
 // Function: buildTree
 //
 
-void buildTree( double *data, int rows, int cols, Tree *tree, MPI_Comm comm, int myRank, int numNodes );
+void buildTree( double *data, int rows, int cols, Tree *tree, MPI_Comm comm, int myRank, int numNodes, string name );
 
 
