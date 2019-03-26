3330eaf739dc6edb97f09a188be27ae35678d0bb
JJ Lay <jj.lay@outlook.com>
2019-03-25 22:55:29 -0500

    Added dumpTree (#76)
    * Sync (#75)
    
    * Fixed memory cast issue in parallelSort with MPI_BCAST (#73)
    
    * Fixed memory cast issue
    
    * Added CSV data (#74)
    
    * Added CSV files
    
    * plot test csv
    
    * plot test csv 2
    
    * convergence bug partly fixed
    
    * convergence maybe fixed
    
    * convergence works and clean cout
    
    * Added dumpTree
    
    * Fixed pointer in dumpTree
    
    * Fixed name in dumpTree
    
    * Added debug
    
    * Added barrier
    
    * dumpTree
    
    * Check for null
    
    * Fixed greater than
    
    * Added rank to dumpTree
    
    * Forgot to add rank to splitting in dumpTree
    
    * Added parent and child
    
    * Fixed semicolons
    
    * Expanded number of nodes
    
    * Added most of the tree attributes to dumpTree
    
    * Added dumpTree to Makefile
    
    * Added defintions to dumpTree
    
    * Forgot reference to t
    
    * Added buildTree source
    
    * Added tree dump files

---
+++ code/kdTree/parallelApproved/dumpTree.cpp
@@ -0,0 +1,60 @@
+
+//
+// Local includes
+//
+
+#include "tree.h"
+#include "definitions.h"
+
+
+//
+// Standard includes
+//
+
+#include <fstream>
+#include <string>
+#include <iostream>
+
+
+//
+// Function: dumpTree
+//
+
+void dumpTree(Tree *t, int depth, ofstream *output, int rank) {
+
+  string padding = "";
+
+  for (auto i = 0; i < depth; i++)
+    padding += "  ";
+
+  *output << padding << "----------" << endl
+    << padding << "Rank: " << rank << endl
+    << padding << "Depth: " << depth << endl
+    << padding << "Name: " << t->name << endl
+    << padding << "Parent: " << (t->p == nullptr ? "none" : t->p->name) << endl
+    << padding << "Left child: " << (t->l == nullptr ? "none" : t->l->name) << endl
+    << padding << "Right child: " << (t->r == nullptr ? "none" : t->r->name) << endl;
+
+  if (t->source == _Source_buildTree_parallel)
+    *output << padding << "Source: buildTree_parallel" << endl;
+  else
+    if (t->source == _Source_buildTree_serial)
+      *output << padding << "Source: buildTree_parallel" << endl;
+    else
+      *output << padding << "Source: unknown" << endl;
+
+  *output << padding << "x-min: " << t->x1 << ", x-max: " << t->x2 << endl
+    << padding << "y-min: " << t->y1 << ", y-max: " << t->y2 << endl
+    << padding << "z-min: " << t->z1 << ", z-max: " << t->z2 << endl
+    << padding << "Node depth: " << t->depth << endl
+    << padding << "Number of points: " << t->n << endl
+    << padding << "Center of node: " << t->c[_X_] << ", " << t->c[_Y_] << ", " << t->c[_Z_] <<endl
+    << padding << "Radius: " << t->radius << endl
+    << padding << "----------" << endl;
+
+  if (t->l != nullptr)
+    dumpTree(t->l, depth+1, output, rank);
+
+  if (t->r != nullptr)
+    dumpTree(t->r, depth+1, output, rank);
+}
