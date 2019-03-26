f95aab477057724a660f439c6c4220ce123cf9e5
JJ Lay <JJ.Lay@tmcat.com>
2019-03-25 22:14:01 -0500

    Check for null

--- code/kdTree/parallelApproved/dumpTree.cpp
+++ code/kdTree/parallelApproved/dumpTree.cpp
@@ -23,7 +23,7 @@ void dumpTree(Tree *t, int depth, ofstream *output) {
   string padding = "";
 
   for (auto i = 0; i < depth; i++)
-    padding += " ";
+    padding += "  ";
 
   cout << padding << "----------" << endl
       << padding << "Depth: " << depth << endl
@@ -35,6 +35,9 @@ void dumpTree(Tree *t, int depth, ofstream *output) {
     << padding << "Name: " << t->name << endl
     << padding << "----------" << endl;
 
-  dumpTree(t->l, depth+1, output);
-  dumpTree(t->r, depth+1, output);
+  if (t->l != nullptr)
+    dumpTree(t->l, depth+1, output);
+
+  if (t-r != nullptr)
+    dumpTree(t->r, depth+1, output);
 }
